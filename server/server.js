c4819a9a1fea1531d112520d26ce0c06df54f762
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-11-10 22:29:53 +0300

    Adding more NPM packages

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
 const authRoutes = require('./routes/auth');
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api', authRoutes);
 
