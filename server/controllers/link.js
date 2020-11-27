d8458061d3c36819597a8a4a73a49d63a726b46a
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-11-27 15:09:48 +0300

    Adding link create and read - server

--- server/controllers/link.js
+++ server/controllers/link.js
@@ -3,11 +3,31 @@ const slugify = require('slugify');
 
 exports.create = (req, res) => {
   const { title, url, categories, type, medium } = req.body;
-  console.table({ title, url, categories, type, medium });
+  const slug = url;
+  let link = new Link({ title, url, categories, type, medium, slug });
+  link.postedBy = req.user._id;
+  // categories
+  let arrayOfCategories = categories && categories.split(',');
+  link.categories = arrayOfCategories;
+
+  link.save((err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'Link already exist' });
+    }
+
+    res.json(data);
+  });
 };
 
 exports.list = (req, res) => {
-  //
+  Link.find({}).exec((err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'Could not list links.' });
+    }
+    res.json(data);
+  });
 };
 
 exports.read = (req, res) => {
