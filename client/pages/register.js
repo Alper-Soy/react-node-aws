441592a01dbe3feb3ee53742618dc5886bba970c
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-12-02 17:30:30 +0300

    Adding choose favourite category on user registration - client

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,33 +1,83 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import Layout from '../components/Layout';
+import Router from 'next/router';
 import axios from 'axios';
-import { showErrorMessage, showSuccessMessage } from '../helpers/alerts';
+import { showSuccessMessage, showErrorMessage } from '../helpers/alerts';
 import { API } from '../config';
 import { isAuth } from '../helpers/auth';
-import Router from 'next/router';
 
 const Register = () => {
   const [state, setState] = useState({
-    name: '',
-    email: '',
-    password: '',
+    name: 'Ryan',
+    email: 'ryan@gmail.com',
+    password: 'rrrrrr',
     error: '',
     success: '',
     buttonText: 'Register',
+    loadedCategories: [],
+    categories: [],
   });
 
-  const { name, email, password, buttonText, success, error } = state;
+  const {
+    name,
+    email,
+    password,
+    error,
+    success,
+    buttonText,
+    loadedCategories,
+    categories,
+  } = state;
 
   useEffect(() => {
     isAuth() && Router.push('/');
   }, []);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  // load categories when component mounts using useEffect
+  useEffect(() => {
+    loadCategories();
+  }, []);
 
+  const loadCategories = async () => {
+    const response = await axios.get(`${API}/categories`);
+    setState({ ...state, loadedCategories: response.data });
+  };
+
+  const handleToggle = (c) => () => {
+    // return the first index or -1
+    const clickedCategory = categories.indexOf(c);
+    const all = [...categories];
+
+    if (clickedCategory === -1) {
+      all.push(c);
+    } else {
+      all.splice(clickedCategory, 1);
+    }
+    console.log('all >> categories', all);
+    setState({ ...state, categories: all, success: '', error: '' });
+  };
+
+  // show categories > checkbox
+  const showCategories = () => {
+    return (
+      loadedCategories &&
+      loadedCategories.map((c, i) => (
+        <li className='list-unstyled' key={c._id}>
+          <input
+            type='checkbox'
+            onChange={handleToggle(c._id)}
+            className='mr-2'
+          />
+          <label className='form-check-label'>{c.name}</label>
+        </li>
+      ))
+    );
+  };
+
+  const handleChange = (name) => (e) => {
     setState({
       ...state,
-      [name]: value,
+      [name]: e.target.value,
       error: '',
       success: '',
       buttonText: 'Register',
@@ -36,15 +86,21 @@ const Register = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.table({
+      name,
+      email,
+      password,
+      categories,
+    });
     setState({ ...state, buttonText: 'Registering' });
-
     try {
       const response = await axios.post(`${API}/register`, {
         name,
         email,
         password,
+        categories,
       });
-
+      console.log(response);
       setState({
         ...state,
         name: '',
@@ -52,15 +108,13 @@ const Register = () => {
         password: '',
         buttonText: 'Submitted',
         success: response.data.message,
-        error: '',
       });
     } catch (error) {
       console.log(error);
       setState({
         ...state,
         buttonText: 'Register',
-        error: error.response.data.error || error.response.data.message,
-        success: '',
+        error: error.response.data.error,
       });
     }
   };
@@ -69,41 +123,44 @@ const Register = () => {
     <form onSubmit={handleSubmit}>
       <div className='form-group'>
         <input
-          className='form-control'
-          required
-          type='text'
-          placeholder='Type your name'
-          name='name'
           value={name}
-          onChange={handleChange}
+          onChange={handleChange('name')}
+          type='text'
+          className='form-control'
+          placeholder='Type your name'
+          required
         />
       </div>
       <div className='form-group'>
         <input
-          className='form-control'
-          required
-          type='email'
-          placeholder='Type your email'
-          name='email'
           value={email}
-          onChange={handleChange}
+          onChange={handleChange('email')}
+          type='email'
+          className='form-control'
+          placeholder='Type your email'
+          required
         />
       </div>
       <div className='form-group'>
         <input
-          className='form-control'
-          required
-          type='password'
-          placeholder='Type your password'
-          name='password'
           value={password}
-          onChange={handleChange}
+          onChange={handleChange('password')}
+          type='password'
+          className='form-control'
+          placeholder='Type your password'
+          required
         />
       </div>
+
       <div className='form-group'>
-        <button className='btn btn-outline-info  btn-block'>
-          {buttonText}
-        </button>
+        <label className='text-muted ml-4'>Category</label>
+        <ul style={{ maxHeight: '100px', overflowY: 'auto' }}>
+          {showCategories()}
+        </ul>
+      </div>
+
+      <div className='form-group'>
+        <button className='btn btn-outline-info'>{buttonText}</button>
       </div>
     </form>
   );
