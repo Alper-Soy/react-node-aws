aae91e6f2aa1f5befff87e4f2fafa9cd12e7e314
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-12-02 17:44:17 +0300

    Adding choose favourite category on user registration - server

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -20,18 +20,17 @@ const ses = new AWS.SES({ apiVersion: '2010-12-01' });
 
 exports.register = (req, res) => {
   // console.log('REGISTER CONTROLLER', req.body);
-  const { name, email, password } = req.body;
-  // check if user already exist in db
+  const { name, email, password, categories } = req.body;
+  // check if user exists in our db
   User.findOne({ email }).exec((err, user) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).send('Server error!');
+    if (user) {
+      return res.status(400).json({
+        error: 'Email is taken',
+      });
     }
-    if (user) return res.status(400).json({ error: 'Email is taken' });
-
-    // generate token with user name, email and password
+    // generate token with user name email and password
     const token = jwt.sign(
-      { name, email, password },
+      { name, email, password, categories },
       process.env.JWT_ACCOUNT_ACTIVATION,
       {
         expiresIn: '10m',
@@ -45,15 +44,15 @@ exports.register = (req, res) => {
 
     sendEmailOnRegister
       .then((data) => {
-        // console.log('email submitted to SES', data);
+        console.log('email submitted to SES', data);
         res.json({
           message: `Email has been sent to ${email}, Follow the instructions to complete your registration`,
         });
       })
       .catch((error) => {
         console.log('ses email on register', error);
-        res.status(error.statusCode).json({
-          message: 'We could not verify your email. Please try again.',
+        res.json({
+          message: `We could not verify your email. Please try again`,
         });
       });
   });
@@ -61,35 +60,48 @@ exports.register = (req, res) => {
 
 exports.registerActivate = (req, res) => {
   const { token } = req.body;
-  jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, (err, decoded) => {
-    // console.log(decoded);
-    if (err) {
-      console.log(err);
-      return res.status(401).json({ error: 'Expired link. Try again' });
-    }
-
-    // const { name, email, password } = jwt.decode(token);
-    const { name, email, password } = decoded;
-    const username = shortId.generate();
-
-    User.findOne({ email }).exec((err, userExist) => {
+  // console.log(token);
+  jwt.verify(
+    token,
+    process.env.JWT_ACCOUNT_ACTIVATION,
+    function (err, decoded) {
       if (err) {
-        console.log(err);
-        return res.status(500).send('Server error!');
+        return res.status(401).json({
+          error: 'Expired link. Try again',
+        });
       }
-      if (userExist) return res.status(400).json({ error: 'Email is taken' });
 
-      const user = new User({ username, name, email, password });
-      user.save((err, user) => {
-        if (err) {
-          console.log(err);
-          return res.status(500).json({ error: 'Server error!' });
+      const { name, email, password, categories } = jwt.decode(token);
+      const username = shortId.generate();
+
+      User.findOne({ email }).exec((err, user) => {
+        if (user) {
+          return res.status(401).json({
+            error: 'Email is taken',
+          });
         }
 
-        return res.json({ message: 'Registration success. Please login!' });
+        // register new user
+        const newUser = new User({
+          username,
+          name,
+          email,
+          password,
+          categories,
+        });
+        newUser.save((err, result) => {
+          if (err) {
+            return res.status(401).json({
+              error: 'Error saving user in database. Try later',
+            });
+          }
+          return res.json({
+            message: 'Registration success. Please login.',
+          });
+        });
       });
-    });
-  });
+    }
+  );
 };
 
 exports.login = (req, res) => {
