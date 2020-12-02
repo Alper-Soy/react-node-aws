7a781b73e8c8489b54ead42e2f66aba26d0dc70b
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-12-02 22:04:17 +0300

    Adding update user profile - server

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 // controllers
-const { read } = require('../controllers/user');
+const { read, update } = require('../controllers/user');
+
+// validators
+const { userUpdateValidator } = require('../validators/auth');
+const { runValidation } = require('../validators');
 
 // middlewares
 const { requireSignin } = require('../middlewares/requireSignin');
@@ -12,5 +16,13 @@ const { adminMiddleware } = require('../middlewares/adminMiddleware');
 
 router.get('/user', requireSignin, authMiddleware, read);
 router.get('/admin', requireSignin, adminMiddleware, read);
+router.put(
+  '/user',
+  userUpdateValidator,
+  runValidation,
+  requireSignin,
+  authMiddleware,
+  update
+);
 
 module.exports = router;
